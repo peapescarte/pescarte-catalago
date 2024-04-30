204dde8dc78ad9f985db4112ec417a2e2e9eda75
Guilherme Rodrigues <guilherme.rodrigues@estudante.ufscar.br>
2024-04-30 11:57:41 -0300

    demo

--- prisma/migrations/20240114210316_suggestion/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `email` to the `suggested_names` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "suggested_names" ADD COLUMN     "email" TEXT NOT NULL;
--- prisma/migrations/20240114210850_suggestion/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "suggested_names" ALTER COLUMN "status" SET DEFAULT 'PENDING';
